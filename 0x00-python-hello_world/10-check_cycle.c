42ec6a774e750f754eee92084a23ff0e2d9a13bd
julien3641 <3641@holbertonschool.com>
2021-12-20 17:59:15 +0100

    task 10 check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a cycle
+ * the node1 is going on 1 step by 1 step
+ * as the second one is going on 2 steps by 2 steps
+ * and so if both are equal the program stop because there is a cycle
+ * @list: list
+ * Return: 0 on failure and 1 on success
+ */
+
+int check_cycle(listint_t *list)
+{
+
+	listint_t *node1, *node2;
+
+	node1 = list->next;
+	node2 = list->next->next;
+	while (node1 != NULL && node2 != NULL)
+	{
+		node1 = node1->next;
+		node2 = node2->next->next;
+		if (node1 == node2)
+			return (1);
+	}
+	return (0);
+}
