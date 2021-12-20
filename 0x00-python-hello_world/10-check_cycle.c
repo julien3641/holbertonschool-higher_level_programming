325fef17fec06c3872cadb86d3b3e36105e6e7d5
julien3641 <3641@holbertonschool.com>
2021-12-20 18:10:08 +0100

    optimisation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,7 +14,7 @@ int check_cycle(listint_t *list)
 
 	listint_t *node1, *node2;
 
-	if (list == NULL || list->next == NULL || list->next->next)
+	if (list == NULL || list->next == NULL || list->next->next == NULL)
 		return (0);
 	node1 = list->next;
 	node2 = list->next->next;
