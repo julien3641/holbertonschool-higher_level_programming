03efee3264a0eea5a4f50939ecd8ce023b2dccdc
julien3641 <3641@holbertonschool.com>
2022-01-03 17:09:59 +0100

    find the largest number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if my_list == "":
-        return None
+    if my_list == []:
+        return
     largest = 0
-    for i in range(len(my_list)):
+    for i in range(0, len(my_list)):
         if (my_list[i] > largest):
             largest = my_list[i]
     return largest
