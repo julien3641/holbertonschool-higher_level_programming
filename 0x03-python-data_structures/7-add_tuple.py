9f37fefaab9c22bd7e52b0a3a03496c51319a06d
julien3641 <3641@holbertonschool.com>
2022-01-03 16:44:36 +0100

    add two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+def tuple_check(tuple_t=()):
+    if len(tuple_t) > 2:
+        tuple_t = (tuple_t[0], tuple_t[1])
+    elif len(tuple_t) <= 1:
+        if len(tuple_t) == 0:
+            tuple_t = (0, 0)
+        if len(tuple_t) == 1:
+            tuple_t = (tuple_t[0], 0)
+    return tuple_t
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) <= 1 and len(tuple_b) <= 1:
-        tuple_a = (tuple_a[0], 0)
-        tuple_b = (tuple_b[0], 0)
-        result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) > 2 and len(tuple_b) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-        tuple_b = (tuple_b[0], tuple_b[1])
-        result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    else:
-        tuple_a = tuple_a + (0, 0)
-        tuple_b = tuple_b + (0, 0)
-        result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_a = tuple_check(tuple_a)
+    tuple_b = tuple_check(tuple_b)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
