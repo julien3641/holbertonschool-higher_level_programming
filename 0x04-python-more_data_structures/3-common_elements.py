72843700c4e424bd0d2677c6733635e5864275da
julien3641 <3641@holbertonschool.com>
2022-01-05 12:19:18 +0100

    find common value of two lists

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return [element for element in set_1 if element in set_2]
+    result = []
+    for element in set_1:
+        if element in set_2:
+            result.append(element)
+    return result
+
+
+   # return [element for element in set_1 if element in set_2]
