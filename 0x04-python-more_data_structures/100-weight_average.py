5cf8b29c0c7e5cd10186342b334548a01e891193
julien3641 <3641@holbertonschool.com>
2022-01-05 18:33:09 +0100

    weighted average of all integer tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == None:
+    if my_list == []:
         return 0
     coeff = 0
     moy = 0
     for i in range(len(my_list)):
         coeff += my_list[i][1]
         moy += my_list[i][0] * my_list[i][1]
-    moyenne = moy / coeff
-    return moyenne
+    return moy / coeff
