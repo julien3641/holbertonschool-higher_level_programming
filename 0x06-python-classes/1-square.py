f3736a2eef42c1d8b9a71bdd81a8e584016fcdcd
julien3641 <3641@holbertonschool.com>
2022-01-19 13:29:18 +0100

    create a size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This function is about to create a class named Square
+Where there will be a private instance attribute size
+
+"""
+
+
+class Square:
+    """create a private instance attribute size"""
+    self __init__(self, size):
+        self._size == size
+
+    @property
+    def size(self):
+        return self.size
