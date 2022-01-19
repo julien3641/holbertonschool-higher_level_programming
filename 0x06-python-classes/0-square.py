760eded6b9b9d7e0e08eea8f09a6f489b9061c38
julien3641 <3641@holbertonschool.com>
2022-01-19 12:22:47 +0100

    with comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,4 +3,5 @@
 """This function is about to create a empty class named Square"""
 
 class Square:
-    pass # An empty class Square
+    """pass is to say nothing in python so the Square is empty"""
+    pass
