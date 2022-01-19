78585866e1d15365f665efcb6bf010d4d4b9ad93
julien3641 <3641@holbertonschool.com>
2022-01-19 19:37:58 +0100

    update conditions and indentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,7 +45,7 @@ class Square:
 
     @position.setter
     def position(self, pos):
-        if type(pos) != tuple or pos[0] < 0 or pos[1] < 0:
+        if type(pos) != tuple or len(position) != 2 or pos[0] < 0 or pos[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = pos
 
