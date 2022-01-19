af76d83400f5031f9873015bd4fe412fc1b1a245
julien3641 <3641@holbertonschool.com>
2022-01-19 14:52:50 +0100

    same as before with raise errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This function is about to create a class named Square
+Where there will be a private instance attribute size
+
+"""
+
+
+class Square:
+    """
+    create a private instance attribute size
+    The __init__ is called when an object is created from the class
+    and it allow the class to initialize the attributes of a class.
+
+    if the size is not a integer, a message occured "size must be an ingeger"
+    if the size is less than 0, a message occured "size must be >=0"
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
