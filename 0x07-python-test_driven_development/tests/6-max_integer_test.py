fec3017c98988c151f84bd63c253a7804019aaa3
julien3641 <3641@holbertonschool.com>
2022-01-24 21:52:02 +0100

    add file unittest task 6 max integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+
+    def test_normal_list(self):
+        list_test = [1, 2, 3, 10]
+        self.assertEqual(max_integer(list_test), 10)
+        list_test = [230, 555, 2902, 287]
+        self.assertEqual(max_integer(list_test), 2902)
+
+    def test_empty(self):
+        list_test = []
+        self.assertIsNone(max_integer(list_test))
+
+    def test_single(self):
+        list_test = [29]
+        self.assertEqual(max_integer(list_test), 29)
+
+    def test_with_string(self):
+        with self.assertRaises(TypeError):
+            list_test2 = [1, "Joe", 3, 10]
+            max_integer(list_test2)
+        with self.assertRaises(TypeError):
+            max_integer(["joe", 4, "5", -6, 9])
+        with self.assertRaises(TypeError):
+            max_integer(2, 9)
+    def test_negatif_number(self):
+        list_test = [5, 4, 3, -2, 1]
+        list_test2 = [-5, -4, -3, 2, -1]
+        list_test3 = [9, 9, -9, -9]
+        self.assertEqual(max_integer(list_test), 5)
+        self.assertEqual(max_integer(list_test2), 2)
+        self.assertEqual(max_integer(list_test3), 9)
