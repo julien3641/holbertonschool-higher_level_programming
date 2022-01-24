18474acf437177c90f170575b4dac6a270aac686
julien3641 <3641@holbertonschool.com>
2022-01-24 21:56:08 +0100

    add comments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,20 +8,24 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_normal_list(self):
+        """ test with normal list"""
         list_test = [1, 2, 3, 10]
         self.assertEqual(max_integer(list_test), 10)
         list_test = [230, 555, 2902, 287]
         self.assertEqual(max_integer(list_test), 2902)
 
     def test_empty(self):
+        """test with an empty list"""
         list_test = []
         self.assertIsNone(max_integer(list_test))
 
     def test_single(self):
+        """test with an single list"""
         list_test = [29]
         self.assertEqual(max_integer(list_test), 29)
 
     def test_with_string(self):
+        """test with strings in the list"""
         with self.assertRaises(TypeError):
             list_test2 = [1, "Joe", 3, 10]
             max_integer(list_test2)
@@ -31,6 +35,7 @@ class TestMaxInteger(unittest.TestCase):
             max_integer(2, 9)
 
     def test_negatif_number(self):
+        """test with negative numbers"""
         list_test = [5, 4, 3, -2, 1]
         list_test2 = [-5, -4, -3, 2, -1]
         list_test3 = [9, 9, -9, -9]
