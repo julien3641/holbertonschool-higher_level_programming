85568739be68193aae30bd8995a6642b6b12a164
julien3641 <3641@holbertonschool.com>
2022-01-25 20:37:15 +0100

    fix code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,16 +13,22 @@ def matrix_divided(matrix, div):
     if the number is not a float change it into an integer
     if the number is nor an integer or a float display the error message"""
 
-new_mat = matrix[:]
-if type(div) != int and (div) != float:
-    raise TypeError("div must be a number")
-if div == 0:
-    raise ZeroDivisionError("division by zero")
-for i in new_mat:
-    if type(new_mat[i]) != int and type(new_mat[i]) != float:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for j in new_mat[i]:
-        if new_mat[i][len(new_mat[i]) != new_mat[i][len(new_mat[i]):
-            result = round(new_matrix[i][j] / div)
-new_mat.append(result)
-return new_mat
+    div_err = "div must be a number"
+    mat_err = "matrix must be a matrix (list of lists) of integers/floats"
+    row_err = "Each row of the matrix must have the same size"
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError(div_err)
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_mat = matrix.copy()
+    for l in range(len(matrix)):
+        new_mat[l] = matrix[l].copy()
+        if l != 0:
+            if len(matrix[l]) != len(matrix[l - 1]):
+                raise TypeError(row_err)
+        for c in range(len(matrix[l])):
+            if type(matrix[l][c]) != int and type(matrix[l][c]) != float:
+                raise TypeError(mat_err)
+            new_mat[l][c] = round(matrix[l][c] / div, 2)
+    return new_mat
