05ff94b62b5acf5b7b7ee60b51f7af6594214206
julien3641 <3641@holbertonschool.com>
2022-01-31 20:17:00 +0100

    requirements pycodestyle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,17 +16,19 @@ the square description: [Square] <width>/<height>
 class Square(Rectangle):
     """ defined a square of size "size by size" """
 
+    """__init__ method to initialize a new instance"""
     def __init__(self, size):
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    """area method to compute the area of the rectangle"""
+
+    def area(self):
+        return self.__size ** 2
 
     """str method to print the rectangle size"""
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
 
-    """area method to compute the area of the rectangle"""
-
-    def area(self):
-        return self.__size ** 2
