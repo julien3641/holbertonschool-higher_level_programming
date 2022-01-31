6181b0a5b7c64932de88025218ac152cd9c62d48
julien3641 <3641@holbertonschool.com>
2022-01-31 12:47:17 +0100

    return if type of obj is a_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write a function that return True of False"""
+
+
+def is_same_class(obj, a_class):
+
+    """ function that return True if the objects is exactly an instance """
+
+    return type(obj) is a_class
