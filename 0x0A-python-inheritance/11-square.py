a3105b87ad56986e7dbd55ef6fe1a90bc78d61a5
julien3641 <3641@holbertonschool.com>
2022-01-31 20:20:03 +0100

    requirements pycodestyle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-Rectangle = __import__('9-rectangle').Rectangle
-
-""" Write a class Square that inherits from Rectangle
-(9-rectangle.py). (task based on 10-square.py).
-Instantiation with size: def __init__(self, size)::
+"""Write a class Square that inherits from Rectangle
 size must be private. No getter or setter
 size must be a positive integer, validated by integer_validator
 the area() method must be implemented
 print() should print, and str() should return
 the square description: [Square] <width>/<height>
 """
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
