dfa10ff508caa3ec87972c763d63e06307125294
julien3641 <3641@holbertonschool.com>
2022-01-31 20:14:47 +0100

    requirements pycodestyle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,12 +2,13 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-""" Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).
+""" Write a class Square that inherits from Rectangle 
+(9-rectangle.py). (task based on 10-square.py).
 Instantiation with size: def __init__(self, size)::
 size must be private. No getter or setter
 size must be a positive integer, validated by integer_validator
 the area() method must be implemented
-print() should print, and str() should return, 
+print() should print, and str() should return
 the square description: [Square] <width>/<height>
 """
 
@@ -27,5 +28,5 @@ class Square(Rectangle):
 
     """area method to compute the area of the rectangle"""
 
-    def erea(self):
+    def area(self):
         return self.__size ** 2
