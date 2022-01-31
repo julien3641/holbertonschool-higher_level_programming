854764ad16a118027b1434f147cc321f01555e8f
julien3641 <3641@holbertonschool.com>
2022-01-31 22:02:15 +0100

    update code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,5 +12,7 @@ def inherits_from(obj, a_class):
     that inherited directly or indirectly from the specified class
     """
 
-    if a_class != bool:
-        return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
