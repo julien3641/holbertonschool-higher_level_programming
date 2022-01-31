27f92a1bb80b7ed005c64e443db87eb5ebd8b657
julien3641 <3641@holbertonschool.com>
2022-01-31 13:39:32 +0100

    task 6 create area with raise message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Write a class BaseGeometry with a public instance"""
+
+
+class BaseGeometry:
+
+    """ def area that raise an exception message"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
