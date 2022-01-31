0b814a56a0fa9b5eb9eefb171a36a5e7fbb79c30
julien3641 <3641@holbertonschool.com>
2022-01-31 15:05:01 +0100

    task 8 initialize the inherant Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Write a class BaseGeometry with a public instance"""
+
+
+class BaseGeometry:
+
+    """ def area that raise an exception message"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    """ public instance method that validate the type of value"""
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    """ write a class Rectangle tha inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
