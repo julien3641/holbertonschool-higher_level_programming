8f4986169d13be913cc2efee74f79ee0023335b6
julien3641 <3641@holbertonschool.com>
2022-02-01 11:54:34 +0100

    return the json representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that returns
+the JSON representation of an object (string):
+Prototype: def to_json_string(my_obj):
+You don’t need to manage exceptions if the object can’t be serialized.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return json.dumps(my_obj)
