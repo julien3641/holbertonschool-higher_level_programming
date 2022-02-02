7f222d40e8fce31b1a79816bfb18bdf2ee5a7f6a
julien3641 <3641@holbertonschool.com>
2022-02-02 13:57:46 +0100

    add file task 13 add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a function that adds a new attribute to an object
+if it’s possible:
+Raise a TypeError exception, with the message
+can't add new attribute if the object can’t have new attribute
+You are not allowed to use try/catch
+You are not allowed to import any module
+"""
+
+
+def add_attribute(obj, name, value):
+    """add a new attribute to an object """
+    if isinstance(obj, (int, float, str, set, list,
+                        dict, tuple, bool, dict, frozenset)):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
