18da3861a02b0b7646755ccfbbc819940e4ad4ce
julien3641 <3641@holbertonschool.com>
2022-02-07 13:10:11 +0100

    update comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-"""Write the class Rectangle that inherits from Base:
-
-    In the file models/rectangle.py
-    Class Rectangle inherits from Base
-    Private instance attributes, each with its own public getter and setter:
-        __width -> width
-        __height -> height
-        __x -> x
-        __y -> y
-    Class constructor: def __init__
-    (self, width, height, x=0, y=0, id=None):
-        Call the super class with id - this super call
-        with use the logic of the __init__ of the Base class
-        Assign each argument width, height, x and y to the right attribute
-
-Why private attributes with getter/setter?
-Why not directly public attribute?
-Because we want to protect attributes of our class.
-With a setter, you are able to validate
-what a developer is trying to assign to a variable.
-So after, in your class you can “trust” these attributes."""
-
+"""
+The module define the class Rectangle.
+"""
 
 from models.base import Base
 
