fc83650a9e567d3a4e17ef67678f64848555c05e
julien3641 <3641@holbertonschool.com>
2022-03-08 13:07:08 +0100

    change code

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,10 +2,6 @@
 -- hbtn_0c_0 in your MySQL server and add multiples rows.
 USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS second_table
-(id INT NOT NULL, name VARCHAR(256) NOT NULL, score INT NOT NULL);
+(id INT, name VARCHAR(256), score INT );
 INSERT INTO second_table (id, name, score)
-VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
