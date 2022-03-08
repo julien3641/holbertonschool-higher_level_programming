d71995c2eb13d89c27fe593d11e41cf84c8e9f77
julien3641 <3641@holbertonschool.com>
2022-03-08 14:07:13 +0100

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 -- script that creates a table called first_table in the current
 -- database in your MySQL server.
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table
-(id INT NOT NULL, name VARCHAR(256) NOT NULL);
+(id INT, name VARCHAR(256));
