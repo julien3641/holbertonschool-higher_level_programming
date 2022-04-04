ef4d694919a622895f15e4aba915ed88db5d53c4
julien3641 <3641@holbertonschool.com>
2022-04-04 15:58:53 +0200

    fix code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints 3 lines with an array and a loop
 
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
