b3c95abac77b3ab859a564eb6800f03367aa8f1e
julien3641 <3641@holbertonschool.com>
2022-04-04 18:06:16 +0200

    task 8 semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,6 +7,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < (parseInt(process.argv[2])); i++) {
-      console.log(myVar.repeat(parseInt(process.argv[2])));
+    console.log(myVar.repeat(parseInt(process.argv[2])));
   }
 }
