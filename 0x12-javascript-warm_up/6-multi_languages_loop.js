58978549737abd5d5b6ecc34f8744c8f5ddb5328
julien3641 <3641@holbertonschool.com>
2022-04-04 15:57:05 +0200

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines with an array and a loop
+
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
