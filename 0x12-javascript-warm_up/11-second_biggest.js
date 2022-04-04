468742c467c48543e70f7eccbcbfe05f0d6a3bc0
julien3641 <3641@holbertonschool.com>
2022-04-04 21:07:38 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list arguments
 
-let biggest = process.argv[2];
-let nextbiggest = process.argv[2];
-
-if (process.argv.length <= 2) {
-    console.log(0);
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > biggest) {
-      nextbiggest = biggest;
-      biggest = process.argv[i];
-    } else if (process.argv[i] > nextbiggest && process.argv[î] !== biggest) {
-      nextbiggest = process.argv[i];
-    }
-  }
+  const args = process.argv.sort();
+  console.log(args.reverse()[1]);
 }
-console.log(nextbiggest);
